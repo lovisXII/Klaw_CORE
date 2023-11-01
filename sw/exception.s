6023a4ce88cc90c511bba107260a49071d25f3fc
LovisXII <logeofpit@yahoo.fr>
2023-11-01 19:35:38 +0100

    Spike works

--- sw/exception.s
+++ sw/exception.s
@@ -1,3 +1,21 @@
+.section .tohost, "aw", @progbits
+
+.globl tohost
+.align 4
+tohost: .dword 0
+
+.globl fromhost
+.align 4
+fromhost: .dword 0
+
+.section .text
+
+write_tohost:
+    li t1, 1
+    la t5, .tohost
+    sw t1,0(t5)
+    j write_tohost
+
 .section .exit
 .global _good
 .global _bad
@@ -10,20 +28,6 @@ _good :
     j write_tohost
 _exception_occur :
     j write_tohost
-
-.space 32
-write_tohost:
-    li t1, 1
-    la t5, .tohost
-    sw	t1,0(t5)
-
-.section .tohost, "aw", @progbits
-.globl tohost
-.align 4
-tohost: .dword 0
-.globl fromhost
-.align 4
-fromhost: .dword 0
 
 .section .kernel
 .global _exception
