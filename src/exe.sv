f0e15f07e37cdaf4a93b9efb3540bd8d9b31e64c
LovisXII <logeofpit@yahoo.fr>
2023-12-05 20:17:23 +0100

    reordering exception and addind ports

--- src/exe.sv
+++ src/exe.sv
@@ -196,10 +196,10 @@ assign st_adr_missaligned   = adr_missaligned &  is_store;
 
 assign adr_fault            = '0;
 
-assign exception_nxt        = pc_missaligned  | instr_access_fault | illegal_inst_q_i
+assign exception_nxt        = (pc_missaligned  | instr_access_fault | illegal_inst_q_i
                             | break_point     | ld_adr_missaligned | ld_access_fault
                             | ld_access_fault | st_adr_missaligned | st_access_fault
-                            | env_call_m_mode;
+                            | env_call_m_mode) & ~flush_v_q & ~flush_v_dly1_q;
 
 assign pc_missaligned_nxt     = pc_missaligned;
 assign instr_access_fault_nxt = instr_access_fault  & ~pc_missaligned_nxt;
