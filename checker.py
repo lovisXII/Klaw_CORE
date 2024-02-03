2b87d2138dab95d3f840ca2b52fa4a4bafa34169
youba213 <feryouba0@gmail.com>
2024-02-04 00:12:18 +0100

    checker.py data format of spike.log

--- checker.py
+++ checker.py
@@ -1,14 +1,54 @@
 spike = 'spike.log'
+test = 'sw/tests/I/add/add_0.S'
+veri = 'a.out.txt.s'
 result = 'result.log'
 
-with open(spike, 'r') as file:
-    # read spike log lines
-    lignes_log = file.readlines()
-# select one line over two
-selected_lines = lignes_log[1::2] 
-
-with open(result, 'w') as output_file:
-    # write those lines in the result file 
-    output_file.writelines(selected_lines)
 
 
+def find_pc_val(test):
+    with open(test, 'r') as file:
+        lignes = file.readlines()
+
+    for i, ligne in enumerate(lignes):
+            if '<_start>:' in ligne:
+                pc = ligne.split()[0] 
+                return pc
+    return -1
+
+def find_start(spike,pc):
+    with open(spike, 'r') as file:
+        # read spike log lines
+        lignes_log = file.readlines()
+    
+    pc = f'0x{pc.lower()}'
+
+    #search pc in the spike log
+    for i, ligne in enumerate(lignes_log):
+        raws = ligne.split()
+        if len(raws) > 2 and raws[2] == pc:
+            with open(result, 'w') as output_file:
+                # write those lines in the result file 
+                for j in range(i, len(lignes_log), 2):
+                    cols = lignes_log[j].split()
+                    # data = raws[4:7]
+                    # output_file.write(' '.join(data) + '\n')
+                    output_file.write(f"Cycle : x, register : {cols[5]} data : {cols[6]}\n")
+
+            print(f"Les lignes ont été écrites dans result.")
+            return
+     
+
+   
+
+    
+
+
+
+pc = find_pc_val(veri)
+
+if pc != -1:
+    print(f"pc = {pc}")
+    find_start(spike,pc)
+    
+else:
+    print("start not found.")
