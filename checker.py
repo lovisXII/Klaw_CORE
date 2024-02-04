ded0a943bbb59017fcc8267299732843daf607c1
youba213 <feryouba0@gmail.com>
2024-02-04 14:48:16 +0100

    update ckecker.py to format spike.log

--- checker.py
+++ checker.py
@@ -5,7 +5,7 @@ result = 'result.log'
 
 
 
-def find_pc_val(test):
+def find_start(test):
     with open(test, 'r') as file:
         lignes = file.readlines()
 
@@ -15,26 +15,42 @@ def find_pc_val(test):
                 return pc
     return -1
 
-def find_start(spike,pc):
+def find_exit(test):
+    with open(test, 'r') as file:
+        lignes = file.readlines()
+
+    for i, ligne in enumerate(lignes):
+            if '<_exit>:' in ligne:
+                pc = ligne.split()[0] 
+                return pc
+    return -1
+
+def result_log(spike,start_,exit_):
     with open(spike, 'r') as file:
         # read spike log lines
         lignes_log = file.readlines()
     
-    pc = f'0x{pc.lower()}'
+    #add "0x" behind the pc values
+    start_ = f'0x{start_.lower()}'
+    exit_ = f'0x{exit_.lower()}'
 
     #search pc in the spike log
     for i, ligne in enumerate(lignes_log):
         raws = ligne.split()
-        if len(raws) > 2 and raws[2] == pc:
+        # search for the start pc value in the 3nd row
+        if len(raws) > 2 and raws[2] == start_:
             with open(result, 'w') as output_file:
-                # write those lines in the result file 
-                for j in range(i, len(lignes_log), 2):
+                # write one line over two from this line 
+                for j in range(i + 1, len(lignes_log), 2):
                     cols = lignes_log[j].split()
-                    # data = raws[4:7]
-                    # output_file.write(' '.join(data) + '\n')
-                    output_file.write(f"Cycle : x, register : {cols[5]} data : {cols[6]}\n")
+                    if len(cols) >= 7:
+                        # output_file.write(f"{cols[3]} la taille de cols {len(cols)}\n")
+                        output_file.write(f"Cycle : x, register : {cols[5]} data : {cols[6]}\n")
+                        # break if exit_ found
+                        if cols[3] >= exit_:
+                            break
 
-            print(f"Les lignes ont été écrites dans result.")
+            print(f"Results are written with success.")
             return
      
 
@@ -44,11 +60,13 @@ def find_start(spike,pc):
 
 
 
-pc = find_pc_val(veri)
+start_ = find_start(veri)
+exit_= find_exit(veri)
 
-if pc != -1:
-    print(f"pc = {pc}")
-    find_start(spike,pc)
+if start_ != -1 and exit_ != -1:
+    print(f"start found at: {start_}\n")
+    print(f"exit found at: {exit_}\n")
+    result_log(spike,start_,exit_)
     
 else:
-    print("start not found.")
+    print("tags not found.")
