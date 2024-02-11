0caca90cc2bc7eca0064682728fc70e6de04666b
LovisXII <logeofpit@yahoo.fr>
2024-02-11 13:49:21 +0100

    [script] Update installation script

--- setup-implem.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-project_root=$PWD
-cd implementation/
-rm -rf OpenLane
-git clone https://github.com/The-OpenROAD-Project/OpenLane
-cd OpenLane
-make
-cd $project_root
--- sw/tests/priviledge/generator/generate_user_test.cpp
+++ sw/tests/priviledge/generator/generate_user_test.cpp
@@ -17,14 +17,14 @@
     The map will store every csr implemented in the architecture and
     the key associated will be the priviledge needed to access it.
 
-    These programm are supposed to be launched after the reset, so cpu 
+    These programm are supposed to be launched after the reset, so cpu
     should be in user mode
 */
 int main(){
     std::map<std::string, int> csr_registers;
 
     // Implemented CSR :
-    
+
     csr_registers.insert({"mvendor_id"  ,MACHINE || READ_ONLY});  // 0xF11, R
     csr_registers.insert({"marchid"     ,MACHINE || READ_ONLY});  // 0xF12, R
     csr_registers.insert({"mimpid"      ,MACHINE || READ_ONLY});  // 0xF13, R
@@ -38,15 +38,15 @@ int main(){
     csr_registers.insert({"mtval"       ,MACHINE || R_W      });  // 0x343, R/W
     csr_registers.insert({"mip"         ,MACHINE || R_W      });  // 0x344, R/W
     csr_registers.insert({"mscratch"    ,MACHINE || R_W      });  // 0x340, R/W
-    
+
     int i = 0;
     for (auto it = csr_registers.begin(); it != csr_registers.end(); ++it)
     {
-         std::string file_name = "../csr_priviledge_access_" + std::to_string(i) + ".S"; 
+         std::string file_name = "../csr_priviledge_access_" + std::to_string(i) + ".S";
          std::ofstream file(file_name);
 
         // Generation of the text for the file
-        
+
          std::string assembly = R"(
 .section .text
 .global _start
@@ -56,7 +56,7 @@ _start :
 
          std::string instruction = R"(
     csrrwi x0,)" + it->first + ",0x1200" ;
-        
+
         if(it-> second && 0b11 == 0b11) // if in machine mode, instruction should not execute
         {
             instruction += R"(
@@ -74,7 +74,7 @@ _start :
          assembly += instruction;
 
         // Writing the file :
-        
+
         file << assembly;
         file.close();
         i++;
