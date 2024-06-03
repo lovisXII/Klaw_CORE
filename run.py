bf50be902e4587bdc74800ba83e729fc5765db3b
LovisXII <logeofpit@yahoo.fr>
2024-06-03 23:08:10 +0200

    [toolchain] remove some args and clean inputs

--- run.py
+++ run.py
@@ -4,11 +4,12 @@ import sys
 import argparse
 from subprocess import run, CalledProcessError, PIPE
 from termcolor import colored
+import checker
 
-def run_subprocess(cmd, message):
+def run_subprocess(cmd, message, env = None):
     print(colored("[INFO]", "green"), message, ":\n", cmd)
     try:
-        result = run(cmd, capture_output=True, text=True, shell=True)
+        result = run(cmd, capture_output=True, text=True, shell=True, env=env)
         if result.stdout.strip():
             print(result.stdout)
         if result.stderr.strip():
@@ -26,19 +27,21 @@ def run_subprocess(cmd, message):
 class Config:
     def __init__(self):
         parser = argparse.ArgumentParser(description="Compile RTL core toolchain")
-        parser.add_argument("--sim"  , action="store_true" , help="Launch the simulation of the RTL")
-        parser.add_argument("--build", action="store_true" , help="Build the the RTL")
-        parser.add_argument("--test"                       , help="File or directory that contains the test files to run")
-        parser.add_argument("--riscof", action="store_true", help="Launch the riscof toolchain")
-        parser.add_argument("--signature"                  , help="Override waves directory, default is logs", default="signature.txt")
-        parser.add_argument("--view"  , action="store_true", help="Launch Gtkwave")
-        parser.add_argument("--impl"  , action="store_true", help="Launch the implementation of using OpenLane")
-        parser.add_argument("--clean" , action="store_true", help="Clean all the generated files")
+        parser.add_argument("--sim"       , action="store_true", help="Launch the simulation of the RTL")
+        parser.add_argument("--build"     , action="store_true", help="Build the the RTL")
+        parser.add_argument("--test"                           , help="File or directory that contains the test files to run")
+        parser.add_argument("--riscof-reg", action="store_true", help="Launch the toolchain riscof")
+        parser.add_argument("--riscof"    , action="store_true", help="Launch the riscof toolchain")
+        parser.add_argument("--impl"      , action="store_true", help="Launch the implementation of using OpenLane")
+        parser.add_argument("--clean"     , action="store_true", help="Clean all the generated files")
 
         args = parser.parse_args()
 
         if (args.sim or args.riscof) and not args.test:
             raise argparse.ArgumentTypeError("--test is required when --sim or --riscof is specified")
+
+        if args.sim and args.riscof and not args.test and not args.run_reg:
+            raise argparse.ArgumentTypeError("--sim, --riscof and --riscof-reg are exclusive")
 
         self.args = args
         self.arch = "rv32izicsr"
@@ -63,14 +66,12 @@ class Config:
         env                       = os.environ.copy()
         env["SYSTEMC_INCLUDE"]    = "/usr/local/systemc-2.3.3/include/"
         env["SYSTEMC_LIBDIR"]     = "/usr/local/systemc-2.3.3/lib-linux64/"
-        run_subprocess(verilator_cmd, "Compile RTL using verilator")
+        run_subprocess(verilator_cmd, "Compile RTL using verilator", env)
 
     def compile_c(self):
-        os.chdir("obj_dir")
         num_cores   = os.cpu_count()
-        make_cmd    = f'make -j {str(num_cores)} -f Vcore.mk'
+        make_cmd    = f'make -j {str(num_cores)} -C obj_dir -f Vcore.mk'
         run_subprocess(make_cmd, "Compile c++")
-        os.chdir("..")
 
     def compile_test(self):
         riscv_gcc   = "/opt/riscv/bin/riscv32-unknown-elf-gcc"
@@ -111,36 +112,47 @@ class Config:
         elif self.args.riscof :
              testname = config.args.test
         spike_cmd   = f"spike -p1 --log=spike.log --priv=m {arch} --log-commits {testname}"
-        checker_cmd = f"python3 ./checker.py"
         run_subprocess(spike_cmd, "Run spike")
-        run_subprocess(checker_cmd, "Run checker")
+        checker.run_checker()
 
     def gtkwave(self):
         cmd = f"gtkwave {config.waves_dir}/vlt_dump.vcd &"
         run_subprocess(cmd, "Running Gtkwave")
 
+    def build_reg(self) :
+        cmd = "./riscof/lanch-riscof.sh build"
+        run_subprocess(cmd, "Building riscof")
+
+    def run_reg(self) :
+        cmd = "./riscof/lanch-riscof.sh run"
+        run_subprocess(cmd, "Running riscof")
+
 if __name__ == "__main__":
 
     config = Config()
 
-    if config.args.build:
+    # Check build argument
+    if config.args.build and config.args.sim :
         config.compile_verilator()
         config.compile_c()
-
-    if config.args.sim:
         config.compile_test()
+    elif config.args.build and config.args.riscof :
+        config.compile_verilator()
+        config.compile_c()
+    elif config.args.build and config.args.riscof_reg :
+        config.build_reg()
+
+    # Run simulation
+    if config.args.sim:
         config.run_simu()
         config.run_checker()
     elif config.args.riscof:
-        config.compile_verilator()
-        config.compile_c()
         config.run_riscof()
         config.run_checker()
+    elif config.args.riscof_reg :
+        config.run_reg()
     elif config.args.clean:
         cmd = "rm -rf obj_dir/ *.vcd *.out.txt.s *.out kernel *.txt *.o *.log obj_dir"
         run_subprocess(cmd, "Cleaning generated files...")
         cmd = "rm -rf implementation/OpenLane/designs/core/src/*.v"
         run_subprocess(cmd, "Cleaning implementation files...")
-
-    if config.args.view:
-        config.gtkwave()
