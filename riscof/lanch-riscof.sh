8a86e93d8e2d21d7d986ead2e194e783bdceb540
LovisXII <logeofpit@yahoo.fr>
2024-06-11 22:27:28 +0200

    [scripts] Change scripts behavior to add KLAW_ROOT

--- riscof/lanch-riscof.sh
+++ riscof/lanch-riscof.sh
@@ -11,16 +11,21 @@ if [ $# -lt 1 ]; then
   usage
 fi
 
+if [ -z $KLAW_ROOT ]; then
+  echo "[ERROR] KLAW_ROOT not found, please source setup.sh first"
+  exit
+fi
+
 cd $KLAW_ROOT
 # Iterate through the arguments using a for loop
 for arg in "$@"; do
   case "$arg" in
     "build")
-      riscof validateyaml --config=config.ini
-      riscof testlist --config=config.ini --suite=riscv-arch-test/riscv-test-suite/ --env=riscv-arch-test/riscv-test-suite/env
+      riscof validateyaml --config=$KLAW_ROOT/riscof/config.ini
+      riscof testlist --config=$KLAW_ROOT/riscof/config.ini --suite=$KLAW_ROOT/riscof/riscv-arch-test/riscv-test-suite/ --env=$KLAW_ROOT/riscof/riscv-arch-test/riscv-test-suite/env
       ;;
     "run")
-      riscof run --config=config.ini --suite=riscv-arch-test/riscv-test-suite/ --env=riscv-arch-test/riscv-test-suite/env
+      riscof run --config=$KLAW_ROOT/riscof/config.ini --suite=$KLAW_ROOT/riscof/riscv-arch-test/riscv-test-suite/ --env=$KLAW_ROOT/riscof/riscv-arch-test/riscv-test-suite/env
       ;;
     *)
       echo "Invalid argument: $arg"
